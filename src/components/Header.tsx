7b7bec43685fb3a1eb9df5ed6ae9ea06f2072d58
jojosuelobo <josuelobo2000@gmail.com>
2024-08-26 17:28:44 -0300

    final header + fav icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,6 +3,12 @@
 import Link from "next/link";
 import { usePath } from "@/app/[locale]/hooks/usePath";
 
+import Image from "next/image";
+import linkedin from '@/svg/iconLinkedin.svg'
+import github from '@/svg/iconGithub.svg'
+import instagram from '@/svg/iconInstagram.svg'
+import youtube from '@/svg/iconYoutube.svg'
+
 export const Header = () => {
     const general = 'text-slate-300';
     const current = 'text-RED font-bold';
@@ -12,13 +18,38 @@ export const Header = () => {
     return (
         <header className="flex justify-between items-center px-6 py-4 mb-5">
             <h2 className="text-RED font-bold">Josué Lobo</h2>
-            <nav className="flex gap-6 items-center">
-                <Link href="/" className={isCurrentPage('/') ? current : general}>Home</Link>
+            <nav className="flex gap-6 items-center p-2">
+                <Link href="/" className={`${isCurrentPage('/') ? current : general}`}>
+                    Home
+                </Link>
 
-                <Link href="/">Blog</Link>
+                <Link href="/">
+                    Blog
+                </Link>
 
-                <Link href="/recomendations" className={isCurrentPage('/recomendations') ? current : general}>Recomendações</Link>
+                <Link href="/recomendations" className={`${isCurrentPage('/recomendations') ? current : general}`}>
+                    Recomendações
+                </Link>
+                <nav className="flex gap-4 items-center p-2">
+                    <Link href="https://www.linkedin.com/in/jojosuelobo/" target="_blank">
+                        <Image src={linkedin} alt="LinkedIn" />
+                    </Link>
+
+                    <Link href="https://github.com/jojosuelobo" target="_blank">
+                        <Image src={github} alt="Github" />
+                    </Link>
+
+                    <Link href="https://www.instagram.com/jojosuelobo/" target="_blank">
+                        <Image src={instagram} alt="Instagram" />
+                    </Link>
+
+                    <Link href="https://www.youtube.com/@jojosueloboYT" target="_blank">
+                        <Image src={youtube} alt="Youtube" />
+                    </Link>
+                </nav>
+
             </nav>
+
         </header>
     );
 };
