8fc2e0f1b84e019e366d2d724c6790f7043dd879
jojosuelobo <josuelobo2000@gmail.com>
2024-08-27 15:53:10 -0300

    remove english version

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,21 +1,13 @@
 import Image from "next/image";
-
-import initTranslations from "@/i18n";
-import TranslationsProvider from "@/components/TranslationsProvider";
-import LanguageChanger from "@/components/LanguageChanger";
-
 import { Header } from "@/components/Header";
 
-const i18nNamespaces = ["home"];
 
-export default async function Home({ params: { locale } } : { params: { locale: any } }) {
-  const { t, resources } = await initTranslations(locale, i18nNamespaces);
+export default function Home() {
+  //const { t, resources } = await initTranslations(locale, i18nNamespaces);
   return (
     <>
-      <TranslationsProvider resources={resources} locale={locale} namespaces={i18nNamespaces}>
-        <Header locale={ locale }/>
-        
-      </TranslationsProvider>
+      <Header />
+
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,6 @@
-"use client";
+'use client'
 
-import initTranslations from "@/i18n";
-import TranslationsProvider from "@/components/TranslationsProvider";
-import LanguageChanger from "@/components/LanguageChanger";
+import { useState } from "react"; // Importa useState para controle do menu móvel
 import Link from "next/link";
 import { usePath } from "@/app/[locale]/hooks/usePath";
 import Image from "next/image";
@@ -11,51 +9,59 @@ import github from "@/svg/iconGithub.svg";
 import instagram from "@/svg/iconInstagram.svg";
 import youtube from "@/svg/iconYoutube.svg";
 
-const i18nNamespaces = ["header"];
-
-export const Header = async ({ locale } : { locale: any }) => {
-    const general = 'text-slate-300';
-    const current = 'text-RED font-bold';
+export const Header = () => {
+    const general = "text-slate-300";
+    const current = "text-RED font-bold";
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false); // Estado para controlar o menu móvel
 
     const { isCurrentPage } = usePath();
-    const { t, resources } = await initTranslations(locale, i18nNamespaces);
 
     return (
-        <TranslationsProvider resources={resources} locale={locale} namespaces={i18nNamespaces}>
-            <header className="flex justify-between items-center px-6 py-4 mb-5 bg-HeaderBG">
+
+        <header className="flex flex-col md:flex-row justify-between items-center px-6 py-4 mb-5 bg-HeaderBG">
+            <div className="flex justify-between w-full md:w-auto items-center">
                 <h2 className="text-RED font-bold text-2xl">Josué Lobo</h2>
-                <nav className="flex gap-6 items-center p-2">
-                    <Link href="/" className={`${isCurrentPage('/') || isCurrentPage('/en') ? current : general}`}>
-                        {t("optionHome")}
+                <button
+                    className="md:hidden text-white"
+                    onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+                >
+                    {isMobileMenuOpen ? <a>Fechar</a> : <a>Abrir</a>}
+                </button>
+            </div>
+
+            <nav className={`flex-col md:flex-row flex ${isMobileMenuOpen ? "flex" : "hidden"} md:flex gap-6 items-center p-2 w-full md:w-auto`}>
+                <Link href="/" className={`${isCurrentPage("/") || isCurrentPage("/en") ? current : general}`}>
+                    Home
+                </Link>
+
+                <Link href="/" className={`${isCurrentPage("/blog") || isCurrentPage("/en/blog") ? current : general}`}>
+                    Blog
+                </Link>
+
+                <Link href="/recomendations" className={`${isCurrentPage("/recomendations") || isCurrentPage("/en/recomendations") ? current : general}`}>
+                    Recomendações
+                </Link>
+
+                <div className="flex gap-4 items-center p-2">
+                    <Link href="https://www.linkedin.com/in/jojosuelobo/" target="_blank">
+                        <Image src={linkedin} alt="LinkedIn" />
                     </Link>
 
-                    <Link href="/" className={`${isCurrentPage('/blog') || isCurrentPage('/en/blog') ? current : general}`}>
-                        {t("optionBlog")}
+                    <Link href="https://github.com/jojosuelobo" target="_blank">
+                        <Image src={github} alt="Github" />
                     </Link>
 
-                    <Link href="/recomendations" className={`${isCurrentPage('/recomendations') || isCurrentPage('/en/recomendations') ? current : general}`}>
-                        {t("optionRecomendations")}
+                    <Link href="https://www.instagram.com/jojosuelobo/" target="_blank">
+                        <Image src={instagram} alt="Instagram" />
                     </Link>
-                    <div className="flex gap-4 items-center p-2">
-                        <Link href="https://www.linkedin.com/in/jojosuelobo/" target="_blank">
-                            <Image src={linkedin} alt="LinkedIn" />
-                        </Link>
 
-                        <Link href="https://github.com/jojosuelobo" target="_blank">
-                            <Image src={github} alt="Github" />
-                        </Link>
+                    <Link href="https://www.youtube.com/@jojosueloboYT" target="_blank">
+                        <Image src={youtube} alt="Youtube" />
+                    </Link>
+                </div>
+                {/* <LanguageChanger /> */}
+            </nav>
+        </header>
 
-                        <Link href="https://www.instagram.com/jojosuelobo/" target="_blank">
-                            <Image src={instagram} alt="Instagram" />
-                        </Link>
-
-                        <Link href="https://www.youtube.com/@jojosueloboYT" target="_blank">
-                            <Image src={youtube} alt="Youtube" />
-                        </Link>
-                    </div>
-                    <LanguageChanger/>
-                </nav>
-            </header>
-        </TranslationsProvider>
     );
 };
